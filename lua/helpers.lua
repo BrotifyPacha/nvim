3b692c5bd47c4cdb4b9c986d6eb0abcc1adb857e
Pavel Gusev <pgusev@ooo-ntl.ru>
2021-12-29 14:51:49 +0300

    Add helper func to exec lua code in visual mode
    And fix <leader>de dap mapping with it

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,8 +1,18 @@
 
 local M = {}
 
-function M.unrequire(m)
+function M.reload(m)
     package.loaded[m] = nil
+    require(m)
+end
+
+function M.visualExec(func)
+    if vim.fn.mode() ~= 'v' or vim.fn.mode() ~= 'V' then
+        vim.api.nvim_exec([[
+            normal! gv
+            lua ]] .. func
+        , false)
+    end
 end
 
 return M
