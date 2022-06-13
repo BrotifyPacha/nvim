202c00c3935e9660ccb6e8672a5fae7eff9abdae
brotifypacha <brotifypacha@gmail.com>
2022-06-13 20:44:43 +0300

    Refresh fugitive status on hunk updates and file writes

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -5,3 +5,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         require('vim.highlight').on_yank({higroup='PmenuSel', timeout=250})
     end,
 })
+
+local refresh_fugitive_callback = 'fugitive#ReloadStatus'
+vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = '*',
+    callback = refresh_fugitive_callback,
+})
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'PachaHunkStatusChanged',
+    nested = true,
+    callback = refresh_fugitive_callback,
+})
