d5b129191d790bf4ee5e4e65c83c84e793f754a6
brotifypacha <brotifypacha@gmail.com>
2022-06-14 14:41:07 +0300

    Group diagnostic signs by severity

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -74,9 +74,7 @@ end
 
 vim.diagnostic.config({
     virtual_text = false,
-    signs = {
-        active = signs
-    },
+    signs = false, --disable signs here to customly display them later
     update_in_insert = true,
     underline = true,
     severity_sort = true,
@@ -85,10 +83,86 @@ vim.diagnostic.config({
         style = "minimal",
         border = "single",
         source = "always",
-        header = "",
-        prefix = "",
+        header = "Diagnostics:",
+        -- prefix = function (d, i, win_total)
+        --     local highlight = 'DiagnosticSignHint'
+        --     if d.severity == 1 then
+        --         highlight = 'DiagnosticSignError'
+        --     elseif d.severity == 2 then
+        --         highlight = 'DiagnosticSignWarn'
+        --     elseif d.severity == 3 then
+        --         highlight = 'DiagnosticSignInfo'
+        --     end
+        --     return i .. '. ', highlight
+        -- end,
     }
 })
+
+-- Create a namespace. This won't be used to add any diagnostics,
+-- only to display them.
+local ns = vim.api.nvim_create_namespace("my_namespace")
+
+-- Create a reference to the original function
+local show = vim.diagnostic.show
+
+local function contains_diagnostic_with_severity(diagnostics, diagnostic)
+    for _, present_diag in pairs(diagnostics) do
+        if present_diag.severity == diagnostic.severity then
+            return true
+        end
+    end
+    return false
+end
+
+local function set_signs(bufnr)
+    -- Get all diagnostics from the current buffer
+    local diagnostics = vim.diagnostic.get(bufnr)
+    -- Find the "worst" diagnostic per line
+    local one_sign_per_severity_per_line = {}
+    for _, d in pairs(diagnostics) do
+        if one_sign_per_severity_per_line[d.lnum] then
+            local present_diags = one_sign_per_severity_per_line[d.lnum]
+            if contains_diagnostic_with_severity(present_diags, d) ~= true then
+                one_sign_per_severity_per_line[d.lnum] = table.insert(present_diags, d)
+            end
+        else
+            one_sign_per_severity_per_line[d.lnum] = {d}
+        end
+    end
+    local filtered_diagnostics = {}
+    print(vim.inspect(one_sign_per_severity_per_line))
+    for _, v in pairs(one_sign_per_severity_per_line) do
+        print(v)
+        for _, j in pairs(v) do
+            print(j)
+            table.insert(filtered_diagnostics, j)
+        end
+    end
+    -- Show the filtered diagnostics using the custom namespace. Use the
+    -- reference to the original function to avoid a loop.
+    show(ns, bufnr, filtered_diagnostics, {
+        virtual_text = false,
+        signs = {
+            active = signs
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "single",
+            source = "always",
+            header = "",
+            prefix = "",
+        }
+    })
+end
+
+function vim.diagnostic.show(namespace, bufnr, ...)
+    show(namespace, bufnr, ...)
+    set_signs(bufnr)
+end
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "single",
