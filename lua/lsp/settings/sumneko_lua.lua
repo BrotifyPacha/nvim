26bc83024bfd5c8480bb08adde97b753d27e25f2
brotifypacha <brotifypacha@gmail.com>
2022-06-20 00:33:02 +0300

    Finetune sumneko_lua config

--- lua/lsp/settings/sumneko_lua.lua
+++ lua/lsp/settings/sumneko_lua.lua
@@ -1,14 +1,22 @@
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
 return {
     settings = {
         Lua = {
+            runtime ={
+                version = 'LuaJIT',
+                path = runtime_path,
+            },
             diagnostics = {
                 globals = { 'vim' },
             },
             workspace = {
                 library = {
-                    [ vim.fn.expand("$VIMRUNTIME/lua") ] = true,
-                    [ vim.fn.stdpath("config") .. '/lua' ] = true,
+                    vim.api.nvim_get_runtime_file("", true),
+                    -- [ vim.fn.expand("$VIMRUNTIME/lua") ] = true,
+                    -- [ vim.fn.stdpath("config") .. '/lua' ] = true,
                 }
             }
         }
