9b4d4802cad2cadaffaf74bb9920cc0f06a0ac57
brotifypacha <brotifypacha@gmail.com>
2024-03-17 14:35:03 +0300

    add linting of proto files

--- after/ftplugin/gitcommit.vim
+++ after/ftplugin/gitcommit.vim
@@ -1,10 +1,16 @@
 lua << EOF
 local branch = vim.fn['FugitiveHead']()
+local prefix = ''
 if string.match(branch, '%u+%-%d+') then
-    local lines = vim.api.nvim_buf_get_lines(0, 0, 99999, false)
-    local branch_line = branch .. ': '
-    vim.api.nvim_set_current_line(branch_line)
-    vim.api.nvim_win_set_cursor(0, { 1, string.len(branch_line) - 1 })
-    vim.api.nvim_command [[ startinsert! ]]
+    prefix = branch .. ': '
 end
 
+if string.match(branch, 'GOHW%-%d+') then
+    branch = string.match(branch, 'GOHW%-%d+')
+    prefix = '[' .. branch .. ']: '
+end
+
+local lines = vim.api.nvim_buf_get_lines(0, 0, 99999, false)
+vim.api.nvim_set_current_line(prefix)
+vim.api.nvim_win_set_cursor(0, { 1, string.len(prefix) - 1 })
+vim.api.nvim_command [[ startinsert! ]]
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -289,6 +289,10 @@ lspconfig.csharp_ls.setup{
   },
 }
 
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS_ON_SAVE = methods.internal.DIAGNOSTICS
 
 local null_ls = require 'null-ls'
 local sources = {
@@ -298,7 +302,38 @@ local sources = {
     diagnostics_postprocess = function (diagnostic)
       diagnostic.severity = vim.diagnostic.severity.WARN
     end
-  })
+  }),
+  h.make_builtin({
+    name = "buf_lint",
+    meta = {
+      url = "https://github.com/bufbuild/buf",
+      description = "A new way of working with Protocol Buffers.",
+    },
+    method = DIAGNOSTICS_ON_SAVE,
+    filetypes = { "proto" },
+    generator_opts = {
+      command = "buf",
+      args = { "lint", "$FILENAME#include_package_files=true" },
+      from_stderr = true,
+      to_stdin = true,
+      format = "line",
+      check_exit_code = function(code)
+        return code <= 1
+      end,
+      on_output = h.diagnostics.from_patterns({
+        {
+          pattern = [[(.*):(%d+):(%d+):(.*)]],
+          groups = { "filename", "row", "col", "message" },
+        },
+        {
+          pattern = [[(.*)]],
+          groups = { "message" },
+        }
+      }),
+    },
+    factory = h.generator_factory,
+})
+  -- null_ls.builtins.diagnostics.protolint,
 }
 
 require 'null-ls'.setup({ sources = sources })
