6f246d38ec205e8058c20f18d40e5adc936a0a8b
brotifypacha <brotifypacha@gmail.com>
2024-03-18 17:29:44 +0300

    proto: add proto.vim and fix null-ls configs

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
 
-local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+local DIAGNOSTICS_ON_SAVE = methods.internal.DIAGNOSTICS_ON_SAVE
 
 local my_protobuf_lint = h.make_builtin({
   name = "buf_lint",
@@ -9,7 +9,7 @@ local my_protobuf_lint = h.make_builtin({
     url = "https://github.com/bufbuild/buf",
     description = "A new way of working with Protocol Buffers.",
   },
-  method = DIAGNOSTICS,
+  method = DIAGNOSTICS_ON_SAVE,
   filetypes = { "proto" },
   generator_opts = {
     command = "buf",
@@ -18,7 +18,7 @@ local my_protobuf_lint = h.make_builtin({
     to_stdin = true,
     format = "line",
     check_exit_code = function(code)
-      print(DIAGNOSTICS)
+      print(DIAGNOSTICS_ON_SAVE)
       return code <= 1
     end,
     on_output = h.diagnostics.from_patterns({
