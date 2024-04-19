d10696798692b6348fbf6ea4414d8a6ef1a73a00
brotifypacha <brotifypacha@gmail.com>
2024-04-19 14:09:38 +0300

    remove unused autocmds

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -37,15 +37,6 @@ vim.api.nvim_create_autocmd('User', {
     callback = refresh_fugitive_callback,
 })
 
-vim.api.nvim_create_augroup('pacha_help_filetype', { clear = true })
-vim.api.nvim_create_autocmd({'BufRead', 'WinEnter'}, {
-    group = 'pacha_help_filetype',
-    pattern = '*\\/doc\\/*.txt',
-    callback = function ()
-        vim.api.nvim_win_set_width(0, 80 + vim.o.numberwidth)
-    end
-})
-
 vim.api.nvim_create_augroup('pacha_recording_status', { clear = true })
 vim.api.nvim_create_autocmd({'RecordingEnter'}, {
     group = 'pacha_recording_status',
@@ -61,37 +52,6 @@ vim.api.nvim_create_autocmd({'RecordingLeave'}, {
         vim.g.recording = false
     end
 })
-vim.api.nvim_create_augroup('custom_session', { clear = true })
-local helpers = require "user.helpers"
-local session = helpers.ExpandEnvs('$HOME/.config/nvim/session')
-local mksession = ':mksession! ' .. session
--- vim.api.nvim_create_autocmd({'VimLeavePre'}, {
---     group = 'custom_session',
---     pattern = '*',
---     callback = function ()
---         vim.cmd(mksession)
---     end
--- })
-vim.api.nvim_create_autocmd({'VimEnter'}, {
-    group = 'custom_session',
-    pattern = '*',
-    callback = function ()
-        if helpers.CheckFileExists(session) then
-            vim.cmd(':source ' .. session)
-        end
-    end
-})
-vim.api.nvim_create_autocmd({'SessionLoadPost'}, {
-    group = 'custom_session',
-    pattern = '*',
-    callback = function ()
-        if helpers.CheckFileExists(session) then
-            os.remove(session)
-            vim.fn.feedkeys([[:bufdo filetype detect]])
-        end
-    end
-})
-vim.cmd('command! SessionSave execute "' .. mksession .. '"' )
 
 vim.api.nvim_create_autocmd({'VimResized'}, {
     pattern = '*',
