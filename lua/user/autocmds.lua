dccf0b800d4a7ec19836e99951b80a4f95546157
brotifypacha <brotifypacha@gmail.com>
2025-03-31 13:56:15 +0300

    autocmds: update highlight on yank to nvim v0.11

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
     callback = function ()
-        require('vim.highlight').on_yank({higroup='PmenuSel', timeout=250})
+        require('vim.hl').on_yank({higroup='PmenuSel', timeout=250})
     end,
 })
 
